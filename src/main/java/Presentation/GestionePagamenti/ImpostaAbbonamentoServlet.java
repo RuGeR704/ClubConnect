cf12d2e18ce59b9229323f5da1a1c7172e835248
Gabdom <ricciardellidomenico69420@gmail.com>
2026-01-05 15:55:44 +0100

    fixato dodelete MetodoPagamento, creata servlet cancella metodo pagamento

--- src/main/java/Presentation/GestionePagamenti/ImpostaAbbonamentoServlet.java
+++ src/main/java/Presentation/GestionePagamenti/ImpostaAbbonamentoServlet.java
@@ -1,4 +1,18 @@
 package Presentation.GestionePagamenti;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+import java.io.IOException;
 
-public class ImpostaAbbonamentoServlet {
+@WebServlet(name = "ImpostaAbbonamentoServlet", value = "/ImpostaAbbonamentoServlet")
+public class ImpostaAbbonamentoServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // TODO: Elabora la richiesta
+    }
+    
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // TODO: Elabora la richiesta
+    }
 }
--- src/main/java/Presentation/GestionePagamenti/PagaRettaServlet.java
+++ src/main/java/Presentation/GestionePagamenti/PagaRettaServlet.java
@@ -1,4 +1,18 @@
 package Presentation.GestionePagamenti;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+import java.io.IOException;
 
-public class PagaRettaServlet {
+@WebServlet(name = "PagaRettaServlet", value = "/PagaRettaServlet")
+public class PagaRettaServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // TODO: Elabora la richiesta
+    }
+    
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // TODO: Elabora la richiesta
+    }
 }
